d8b267cf02f8542237d7039c2f2eae967232c92f
Pawel Dera <paweldera01@gmail.com>
2022-03-28 14:32:06 +0200

    Added all functions for transmission

--- src/main/java/logic/Main.java
+++ src/main/java/logic/Main.java
@@ -1,11 +1,34 @@
 package logic;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
         Transmission T = new Transmission();
 
+        int[] code = {0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0};
+//        int[] encodedCode = T.encodeCharacter(code);
 
-        var a = T.op();
-        System.out.println(T.isCorrect(a));
+        var errorTable = T.getErrorTable(code);
+
+        // check if program has no errors
+        if (T.isCorrect(errorTable)) {
+            System.out.println("There is no errors");
+        }
+        // check if program has one error
+        else if(T.getErrorHCol(errorTable) != -1) {
+            System.out.println("Error is on " + T.getErrorHCol(errorTable) + " bit");
+        }
+        // check if program has 2 errors
+        else if(!Arrays.equals(T.getAddedErrorHCols(errorTable), new int[]{0, 0})) {
+            System.out.println("Error is on " + T.getAddedErrorHCols(errorTable)[0] + " and " + T.getAddedErrorHCols(errorTable)[1] +  " bit");
+        }
+        // there is more errors or given code is not valid
+        else{
+            System.out.println("There is more than 2 errors or given code is not valid");
+        }
+
+        System.out.println("stop");
     }
 }
--- src/main/java/logic/Transmission.java
+++ src/main/java/logic/Transmission.java
@@ -1,16 +1,21 @@
 package logic;
 
+import java.util.Arrays;
+
 public class Transmission {
 
-    int[][] H = {
-                {1, 1, 0, 1, 1, 0, 0},
-                {1, 1, 0, 1, 1, 0, 0},
-                {1, 1, 0, 1, 1, 0, 0}
-            };
+    private int[][] H = {
+            {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0},
+            {1,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0},
+            {1,1,0,1,1,1,0,1,0,0,1,0,0,0,0,0},
+            {1,1,0,1,0,1,0,1,0,0,0,1,0,0,0,0},
+            {1,0,0,1,0,1,0,1,0,0,0,0,1,0,0,0},
+            {1,0,0,1,0,1,0,0,0,0,0,0,0,1,0,0},
+            {1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}
+    };
 
-    int[] code = {0, 0, 0, 1, 1, 1, 1};
-
-    int[] op() {
+    public int[] getErrorTable(int[] code) {
         int[] out = new int[H.length];
 
         for (int i = 0; i < H.length; i++) {
@@ -24,10 +29,84 @@ public class Transmission {
         return out;
     }
 
-    boolean isCorrect(int[] tab) {
-        for (int i = 0; i < tab.length; i++) {
-            if(tab[i] != 0) return false;
+    public int[] encodeCharacter(int[] character) {
+        int[] out = new int[16];
+        int[] controlBits = getErrorTable(character);
+
+        for (int i = 0; i < 8; i++) {
+            out[i] = character[i];
+            out[i+8] = controlBits[i];
+        }
+
+        return out;
+    }
+
+    public int[] getHE(int[] errorTable) {
+        int[] out = new int[H.length];
+
+        for (int i = 0; i < H.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < errorTable.length; j++) {
+                temp += H[i][j] * errorTable[j];
+            }
+            out[i] = temp%2;
+        }
+
+        return out;
+    }
+
+    public boolean isCorrect(int[] errorTable) {
+        for (int i = 0; i < errorTable.length; i++) {
+            if(errorTable[i] != 0) return false;
         }
         return true;
+    }
+
+    public int[] getAddedErrorHCols(int[] errorTable) {
+
+        int[] fcol;
+        int[] scol;
+
+        for (int i = 0; i < H.length; i++) {
+            fcol = getColFromColNumber(i);
+            for (int j = 0; j < H.length; j++) {
+                scol = getColFromColNumber(j);
+
+                if(Arrays.equals(addTwoCols(fcol, scol), errorTable)) return new int[]{i,j};
+            }
+        }
+
+        return new int[]{0,0};
+    }
+
+    public int getErrorHCol(int[] errorTable) {
+        int[] fcol;
+
+        for (int i = 0; i < H.length; i++) {
+            fcol = getColFromColNumber(i);
+            if(Arrays.equals(fcol, errorTable)) return i;
+        }
+
+        return -1;
+    }
+
+    public int[] getColFromColNumber(int colNumber) {
+        int[] output = new int[8];
+
+        for (int i = 0; i < H.length; i++) {
+            output[i] = H[i][colNumber];
+        }
+
+        return output;
+    }
+
+    public int[] addTwoCols(int[] col1, int[] col2) {
+        int[] output = new int[col1.length];
+
+        for (int i = 0; i < col1.length; i++) {
+            output[i] = (col1[i] + col2[i]) % 2;
+        }
+
+        return output;
     }
 }
