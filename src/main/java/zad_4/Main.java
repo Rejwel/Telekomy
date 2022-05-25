79d25b9a5b9b68d7f6d1e37345c79a9fcdfaa92e
230691 Grzelak Filip <230691@edu.p.lodz.pl>
2022-05-25 21:04:28 +0200

    program implementation

--- src/main/java/zad_4/Main.java
+++ src/main/java/zad_4/Main.java
@@ -1,5 +1,6 @@
 package zad_4;
 
+import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -10,13 +11,34 @@ import java.util.Enumeration;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, LineUnavailableException {
         String word = "dupa";
-//        Sender sender = new Sender();
-//        sender.sendBytes(word.getBytes());
-        Receiver receiver = new Receiver("169.254.59.39");
+        Sender sender = new Sender();
+        sender.sendBytes(word.getBytes());
+//        Receiver receiver = new Receiver("169.254.59.39");
+        AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+        TargetDataLine targetLine = (TargetDataLine) AudioSystem.getLine(info);
+        targetLine.open();
+        Thread monitorThread = new Thread() {
+            @Override
+            public void run() {
+                targetLine.start();
 
+                byte[] data = new byte[targetLine.getBufferSize() / 5];
+                System.out.println(targetLine.getBufferSize());
 
+                while (true) {
+                    targetLine.read(data, 0, data.length);
+                    try {
+                        sender.sendBytes(data);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        monitorThread.start();
     }
 
 
--- src/main/java/zad_4/Receiver.java
+++ src/main/java/zad_4/Receiver.java
@@ -1,5 +1,6 @@
 package zad_4;
 
+import javax.sound.sampled.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
@@ -7,6 +8,7 @@ import java.util.Arrays;
 
 public class Receiver {
     private Socket socket;
+    private byte[] bytes = new byte[4];
 
     public Receiver(String address) {
         try {
@@ -14,19 +16,34 @@ public class Receiver {
                 socket = new Socket(address, 5000);
             }
             receivedData();
-        } catch (IOException e) {
+        } catch (IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
     }
 
-    public void receivedData() throws IOException {
+    public void receivedData() throws IOException, LineUnavailableException {
+        AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+        SourceDataLine sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+        sourceLine.open();
+        Thread monitorThread = new Thread() {
+            @Override
+            public void run() {
+                sourceLine.start();
+
+                while (true) {
+                    sourceLine.write(bytes, 0, 4);
+                }
+            }
+        };
+        monitorThread.start();
         InputStream socketIn = socket.getInputStream();
         while(true){
-            byte[] bytes = new byte[4];
+            bytes = new byte[4];
             for (int i =0; i < 4; i++) {
                 bytes[i] = (byte) socketIn.read();
             }
-            System.out.println(Arrays.toString(bytes));
+
         }
     }
 }
